be16b8d0c1b73daa8961b828b61f3a4523b161da
Galina Kostina <Kostina13@ya.ru>
2023-07-05 17:30:56 +0300

    DZ

--- src/Conponents/Header/Navigation/Category/Category.jsx
+++ src/Conponents/Header/Navigation/Category/Category.jsx
@@ -1,15 +1,40 @@
+import { NavLink } from 'react-router-dom';
+import s from './Category.module.scss';
+
+
 const list = [
-{ link: 'bras', title: 'Бюстгальтеры'},
-{ link: 'panties', title: 'Трусы'},
-{ link: 'socks', title: 'Носки'},
-{ link: 'bathrobes', title: 'Халаты'},
-{ link: 'thermsl', title: 'Термобелье'},
-    { link: 'pijamas', title: 'Пижамы'},
+    { 
+        link: 'women', 
+        title: 'Женщины', 
+        categories: [
+        { link: 'bras', title: 'Бюстгальтеры'},
+        { link: 'panties', title: 'Трусы'},
+        { link: 'socks', title: 'Носки'},
+        { link: 'bathrobes', title: 'Халаты'},
+        { link: 'thermsl', title: 'Термобелье'},
+        { link: 'pijamas', title: 'Пижамы'},
+        ]
+    },
+    // { link: 'men', title: 'Мужчины', categories: [
+    //     { link: 'panties', title: 'Трусы'},
+    //     { link: 'socks', title: 'Носки'},
+    //     { link: 'bathrobes', title: 'Халаты'},
+    //     { link: 'thermsl', title: 'Термобелье'},
+    //     ] },
 ];
 
+
 export const Category= () => (
+<div className={s.category}>
+    <ul className={s.category}>
+    {list.categories.map((category => 
+        <li key={category.link}>
+            <NavLink className={s.link} to={`${list.link}/${category.link}`}>
+                {category.title}
+            </NavLink>
+        </li>
+        ))}
+    </ul>
+</div>
     
-    <div>
-        Category
-    </div>
 )
